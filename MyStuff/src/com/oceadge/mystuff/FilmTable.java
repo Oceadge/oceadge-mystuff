d7544a4127fe9c9a143ae64e2e7cce6f0abd246c
Oceadge <Oceadge@be8e5100-ca32-0410-97a5-4fc110babfa0>
2007-10-25 08:25:15 +0000

    Removed the code to connect to the database with just a call to the DataBase class instead.

--- MyStuff/src/com/oceadge/mystuff/FilmTable.java
+++ MyStuff/src/com/oceadge/mystuff/FilmTable.java
@@ -1,7 +1,6 @@
 package com.oceadge.mystuff;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,10 +21,6 @@ import java.sql.Statement;
 
 public class FilmTable {
 
-  final String DATABASE = "jdbc:postgresql://localhost/my_stuff";
-  final String DATABASE_U = "postgres";
-  final String DATABASE_P = "postgres4me";
-
   Connection c = null;
   PreparedStatement ps = null;
   Statement s = null;
@@ -34,45 +29,21 @@ public class FilmTable {
 
 
 
-
-  /*
-   * Method: establishConnection()
-   *
-   */
-  private void establishConnection(){
-    try {
-      Class.forName("org.postgresql.Driver");
-    } catch (ClassNotFoundException cnfe) {
-      System.out.println("Couldn't find the driver!");
-      System.out.println("Let's print a stack trace, and exit.");
-      cnfe.printStackTrace();
-      System.exit(1);
-    }
-
-    try {
-      // The second and third arguments are the username and password,
-      // respectively. They should be whatever is necessary to connect
-      // to the database.
-      c = DriverManager.getConnection(DATABASE, DATABASE_U, DATABASE_P);
-    } catch (SQLException se) {
-      System.out.println("Couldn't connect: print out a stack trace and exit.");
-      se.printStackTrace();
-      System.exit(1);
-    }
-  }
-
 	/*
    * Method: insertFilm()
    *
 	 */
   public boolean insertFilm(String film_name, String film_name2, String film_name_alternative, String film_name_original, int film_year, String film_comments) {
 
-    establishConnection();
+    DataBase d = new DataBase();
+    c = d.establishConnection(); 
     boolean found = false;
+    
     found = testFilmExists(film_name, film_year);
     System.out.println("Found = " + found);
-    if (found == true){
+    if (found){
       System.out.println("Film: " + film_name + " already exists");
+      c.close();
       return true;
     }
 
@@ -94,12 +65,13 @@ public class FilmTable {
 	   	  System.out.println("We got an exception while preparing a statement:" +
 	   	                     "Probably bad SQL.");
 	   	  se.printStackTrace();
-	   	  System.exit(1);
+	   	  System.exit(1);        
 	   	}
 
 	  	try {
 	      ps.executeUpdate();
 	      System.out.println("Added " + film_name + " to table");
+        c.close();
 	      return false;
 	    } catch (SQLException se) {
 	      System.out.println("We got an exception while executing an update:" +
@@ -116,7 +88,8 @@ public class FilmTable {
    */
   public void getAllFilms(){
 
-    establishConnection();
+    DataBase d = new DataBase();
+    c = d.establishConnection();
 
     try {
       s = c.createStatement();
@@ -129,6 +102,7 @@ public class FilmTable {
 
     try {
       rs = s.executeQuery("SELECT * FROM film_table");
+      c.close();
     } catch (SQLException se) {
       System.out.println("We got an exception while executing our query:" +
                          "that probably means our SQL is invalid");
@@ -160,7 +134,7 @@ public class FilmTable {
     s = null;
     rs = null;
 
-    establishConnection();
+    //establishConnection();
 
     try {
       s = c.createStatement();
@@ -197,7 +171,7 @@ public class FilmTable {
 
     //updateString1 = "update Employees set name = 'hemanthbalaji' where Employee_id = 6323";
 
-    establishConnection();
+    //establishConnection();
     int i = 0;
 
     try {
